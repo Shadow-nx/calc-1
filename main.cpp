c325948bbc53442bd3d195997506eef90a7c34d6
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2017-10-27 00:59:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,41 @@
-int main(int argc, char* argv[]) {
+#include <iostream>
 
+using namespace std;
+
+struct Node {
+	int data;
+	Node *next;
+};
+
+int main() {
+
+	//Node *first=new Node {1,nullptr};
+	//Node *second=new Node {2,nullptr};
+	//first->next=second;
+
+	//Node *second=new Node {2,nullptr};
+	//Node *first=new Node {1,second};
+
+	Node * first = new Node;
+	first=nullptr;
+	Node * last = new Node;
+ 	last=nullptr;
+
+	int n=0;
+	while(cin>>n) {
+		Node *node=new Node {n,nullptr};
+		node->data=n;
+		node->next=first;
+		first = node;
+	}
+
+	Node *curr=first;
+	do {
+		cout<<curr->data;
+		if(curr->next!=nullptr)
+			cout<<" -> ";
+		curr=curr->next;
+	} while (curr!=nullptr);
+
+	return 0;
 }
